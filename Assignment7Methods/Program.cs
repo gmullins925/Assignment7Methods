aa96ead3875e7b90a0347b892148900ee65e1c6b
Gary Mullins <gmullins925@outlook.com>
2019-08-25 23:55:36 -0500

    Commit 3

--- Assignment7Methods/Program.cs
+++ Assignment7Methods/Program.cs
@@ -8,42 +8,66 @@ namespace Assignment7Methods
 {
 	class Program
 	{
-		static void Main(string[] args)
+		static void Main()
 		{
 			Random r = new Random();
-
 			int val = r.Next(1, 100);
 			int guess = 0;
-			bool correct = false;
+			string yesNo;
 
-			Console.WriteLine("I'm thinking of a number between 1 and 100.");
-
-			while (!correct)
+			do
 			{
-				Console.Write("Your Guess: ");
-				string input = Console.ReadLine();
+				Console.WriteLine("Hey, You! Guess a number between 1 and 100!");
 
-				if (!int.TryParse(input, out guess))
+				while (guess != val)
 				{
-					Console.WriteLine("That's not a number.");
-					continue;
+
+					Console.Write("Your Guess: ");
+					string input = Console.ReadLine();
+
+					if (!int.TryParse(input, out guess))
+					{
+						Console.WriteLine("That's not a number.");
+						continue;
+					}
+					if (guess < val)
+					{
+						Console.WriteLine("My number is higher than " + input);
+					}
+					else if (guess > val)
+					{
+						Console.WriteLine("My number is lower than " + input);
+					}
+					else
+					{
+						Console.WriteLine("You are correct! (:");
+					}
+				}
+				Console.WriteLine("Press 'e' to exit or 'y' to play again");
+
+				yesNo = Console.ReadLine().ToLower();
+
+				while (!yesNo.Equals("y") && !yesNo.Equals("e"))
+				{
+
+					Console.WriteLine("Invalid!");
+
+					Console.WriteLine("Press 'e' to exit or 'y' to play again");
+
+					yesNo = Console.ReadLine().ToLower();
+
 				}
 
-				if (guess < val)
-				{
-					Console.WriteLine("No, the number I'm thinking of is higher.");
-				}
-				else if (guess > val)
-				{
-					Console.WriteLine("No, the number I'm thinking of is lower.");
-				}
-				else
-				{
-					correct = true;
-					Console.WriteLine("You guessed right!");
-					Console.ReadLine();
-				}
 			}
+			while (yesNo.Equals("e"));
+			{
+				Main();
+
+			}
+			while (yesNo.Equals("y"));
+
+			System.Environment.Exit(0);
+			}
+
 		}
 	}
-}
